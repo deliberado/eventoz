f09891df9db5bd77f5c4e3fec10698dbf8f3752d
Fabio Castro <deliberado@gmail.com>
2018-05-23 16:51:52 -0300

    Inicio banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask('primeira_app', template_folder='templates')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+
+class Evento(db.Model):
+	_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	nome = db.Column(db.String)
+	sigla = db.Column(db.String)
+
+
+db.create_all()
+
 
 @app.route("/")
 def index():
@@ -12,5 +28,6 @@ def lista_eventos():
     lista = ['ENCOINFO', 'CAOS', 'Jornanda de Iniciação Cientifica']
     return render_template('lista_eventos.html', eventos=lista)
 
-app.run(debug=True, use_reloader=True)
 
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
