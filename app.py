5b2044ddcc7d44fa721399daacaa26729f1a66fe
Fabio Castro <deliberado@gmail.com>
2018-05-23 15:35:57 -0300

    Herança de templates

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def index():
     return render_template('base.html')
 
 
+@app.route("/eventos")
+def lista_eventos():
+    return render_template('lista_eventos.html')
+
 app.run(debug=True, use_reloader=True)
 
